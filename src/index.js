0fb17b7157cc15ed2eef60d52b2e61942288631c
Pavel Zhachko <rammiel@yandex.ru>
2020-09-30 03:11:35 +0300

    feat: add functional for towel sort function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) return [];
+  if (matrix.length === 0) return [];
+  matrix = matrix.map((el, index) => {
+    if (index % 2 === 0) {
+      return el.sort((a,b) => a-b)
+    } else {
+      return el.sort((a,b) => b-a)
+    }
+  })
+  return matrix.flat()
 }
